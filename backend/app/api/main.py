13be82f331c06858379ed85ffb3f1a17841ac85e
ngynht <nguyenthihongtruc29@gmail.com>
2025-11-07 20:43:08 +0700

    Thêm frontend và cài cors cho main.py

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.router_chat import router as chat_router
 from app.service.router_map import router as map_router
 from app.service.router_weather import router as weather_router
@@ -8,6 +9,21 @@ app = FastAPI(
     description="Backend cho ứng dụng hướng dẫn du lịch tích hợp Google Maps và Thời tiết."
 )
 
+# --- CORS ---
+origins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],   # GET, POST, OPTIONS, PUT, DELETE
+    allow_headers=["*"]
+)
+
+# --- Routers ---
 app.include_router(chat_router)
 app.include_router(map_router)
 app.include_router(weather_router)
