b1382ab19adb564dd80cac6038fe7d01472e2f0a
Nguyen Nam <nghoofnam@gmail.com>
2025-11-30 21:08:26 +0700

    feat: add tourism router
    - Added tourism API endpoint

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -4,15 +4,18 @@ from app.service.router_map import router as map_router
 from app.service.router_weather import router as weather_router
 from app.service.food_router import router as food_router
 from app.service.hotel_router import router as hotel_router
+from app.service.tourism.tourism_router import router as tourism_router
+
 app = FastAPI(
     title="Smart Tourism System",
-    description="Backend cho ứng dụng hướng dẫn du lịch tích hợp Google Maps và Thời tiết."
+    description="Backend cho ứng dụng hướng dẫn du lịch."
 )
 
 @app.get("/")
 def home():
     return {"message": "🎉 Vietnam Smart Tourism API đang hoạt động!"}
 
+app.include_router(tourism_router)
 app.include_router(chat_router)
 app.include_router(map_router)
 app.include_router(weather_router)
