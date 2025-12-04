b0a9daa0116aa4894b944fcef0126708764277a7
Tony <dangthetony1707@gmail.com>
2025-12-04 21:24:58 +0700

    feat: update service api foods

---
+++ backend/app/service/foods/food_module.py
@@ -0,0 +1,42 @@
+import os
+import json
+
+DATA_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "..", "..", "..", "data", "vietnam_foods.jsonl"
+)
+
+DATA_PATH = os.path.abspath(DATA_PATH)
+
+def load_foods():
+    foods = []
+    with open(DATA_PATH, "r", encoding="utf-8") as f:
+        for line in f:
+            foods.append(json.loads(line))
+    return foods
+
+FOODS = load_foods()
+
+def get_main_tags(province: str):
+    tags_counter = {}
+
+    for item in FOODS:
+        if item["province"] != province:
+            continue
+        for tag in item["tags"]:
+            tags_counter[tag] = tags_counter.get(tag, 0) + 1
+
+    sorted_tags = sorted(tags_counter.items(), key=lambda x: x[1], reverse=True)
+    return [tag for tag, _ in sorted_tags[:6]]
+
+
+def get_foods_by_province_and_tag(province: str, tag: str):
+    return [
+        {
+            "food": item["food"],
+            "description": item["description"]
+        }
+        for item in FOODS
+        if item["province"] == province and tag in item["tags"]
+    ]
+
